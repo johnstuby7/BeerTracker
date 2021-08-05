0f6ceff790b8ef2950a88810c672510fb4658657
jstuby <johnstuby7@gmail.com>
2021-08-05 10:09:47 -0600

    Made a basic grid on home page to display beers

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,38 @@
 import React from "react";
 import { Container, Row, Col } from 'react-grid-system';
+import { Grid, GridColumn as Column } from "@progress/kendo-react-grid";
+import beers from "./beers.json";
 
 function Home() {
   return (
     <Container>
-      <Row>
-        <Col sm={4}>
-          One of three columns
-        </Col>
-        <Col sm={4}>
-          One of three columns
-        </Col>
-        <Col sm={4}>
-          One of three columns
-        </Col>
-      </Row>
+      <div>
+      <Grid
+      style={{
+        height: "400px",
+      }}
+      data={beers}
+    >
+      <Column field="BeerID" title="ID" width="40px" />
+      <Column field="BeerName" title="Name" width="250px" />
+      <Column field="type" title="Type" width="250px" />
+      <Column field="rating" title="Rating" width="250px" />
+      <Column field="location" title="Location" width="250px" />
+      <Column field="description" title="Description" width="500px" />
+      <Column width="250px"
+        field="Discontinued"
+        cell={(props) => (
+          <td>
+            <input
+              disabled={true}
+              type="checkbox"
+              checked={props.dataItem[props.field || ""]}
+            />
+          </td>
+        )}
+      />
+    </Grid>
+      </div>
     </Container>
   );
 }
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,7 +7,7 @@ function Navigation(props) {
             <nav class="navbar navbar-expand navbar-dark bg-dark">
             <div class="container">
                 <Link class="navbar-brand" to="/">
-                    React Multi-Page Website
+                    John Stuby Beer Tracker
                 </Link>
                 <div>
                     <ul class="navbar-nav ml-auto">
